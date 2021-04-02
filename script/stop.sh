95858e3ac69782fff7c46434760f8001f3025f11
Tom <tom@gmail.com>
2021-04-02 16:02:35 +0700

    调整启动脚本

--- script/stop.sh
+++ script/stop.sh
@@ -6,7 +6,7 @@ source ./config.sh $1
 num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep -v "grep" | wc -l)
 if [ $num -gt 0 ]; then
   ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${ACTIVE}" | grep "${CLIENT_ID}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
+  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >> "${PROJECT_CONSOLE_LOG_PATH}/${ACTIVE}.${PROJECT_NAME}.log"
 else
-  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${PROJECT_NAME}.log"
+  echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >> "${PROJECT_CONSOLE_LOG_PATH}/${ACTIVE}.${PROJECT_NAME}.log"
 fi
