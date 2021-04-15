641acea8c8d89e9cd2f9c92179b3c2c70fd19d47
Tom <tom@gmail.com>
2021-04-16 01:32:00 +0700

    启动脚本调整

--- script/config.sh
+++ script/config.sh
@@ -26,23 +26,23 @@ fi
 # 启动环境，默认dev
 active="dev"
 client_id="7777"
-client_config=("127.0.0.1:8083")
+server_config=("127.0.0.1:7496")
 if [ "$1" == "-prod" ]; then
   active="prod"
   client_id="70001"
-  client_config=("10.0.2.27:8827")
+  server_config=("10.0.2.27:7496")
 elif [ "$1" == "-pre" ]; then
   active="pre"
   client_id="10001"
-  client_config=("10.0.2.27:8827")
+  server_config=("10.0.2.27:7496")
 elif [ "$1" == "-test" ]; then
-  client_id="40001"
+  client_id="70001"
   active="test"
 fi
 
 echo "active: ${active}"
 echo "client_id: ${client_id}"
-echo "client_config: ${#client_config[@]}"
+echo "server_config: ${#server_config[@]}"
 
 # 启动分组
 group_num=3 # 当前股票分组数量
@@ -55,10 +55,10 @@ tmp_client_id=$((client_id + i))
 CONFIG[$i]="${tmp_client_id}:stock_static_symbol_ind"
 
 
-client_config_len=${#client_config[@]}
+server_config_len=${#server_config[@]}
 for i in "${!CONFIG[@]}"; do
-  mod=`expr $i % $client_config_len`
-  CONFIG[$i]="${active}:${client_config[$mod]}:${CONFIG[$i]}"
+  mod=`expr $i % $server_config_len`
+  CONFIG[$i]="${active}:${server_config[$mod]}:${CONFIG[$i]}"
   echo "CONFIG: ${CONFIG[$i]}"
 done
 
--- script/start.sh
+++ script/start.sh
@@ -5,7 +5,7 @@ source ./config.sh $1
 for item in ${CONFIG[@]};
 do
   active=`echo ${item} | awk -F ':' '{print $1}'`
-  client_host=`echo ${item} | awk -F ':' '{print $2}'`
+  server_host=`echo ${item} | awk -F ':' '{print $2}'`
   server_port=`echo ${item} | awk -F ':' '{print $3}'`
   client_id=`echo ${item} | awk -F ':' '{print $4}'`
   redis_key=`echo ${item} | awk -F ':' '{print $5}'`
@@ -13,17 +13,17 @@ do
   # 启动服务
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${active}" | grep "${client_id}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: is running." >> "${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}.${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >> "${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}.${PROJECT_NAME}.log"
 
     time_hour=$(date "+%Y%m%d%H%M");
-    log_path="${PROJECT_CONSOLE_LOG_PATH}/${client_host}.${client_id}.${time_hour}.log"
+    log_path="${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}.${time_hour}.log"
     nohup java -jar ${PROJECT_NAME_JAR} \
     --spring.profiles.active=${active} \
     --my.ib.server.clientId=${client_id} \
-    --my.ib.server.host=${client_host} \
-    --server.port=${server_port} \
+    --my.ib.server.host=${server_host} \
+    --my.ib.server.port=${server_port} \
     --my.stock.redisKey=${redis_key} \
     >> ${log_path} 2>&1 &
   fi
