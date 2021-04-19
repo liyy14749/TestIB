8462a716b5af4a99468e3f6bc439824e1f1ef6fc
Tom <tom@gmail.com>
2021-04-19 12:47:59 +0700

    启动脚本调整

--- script/start.sh
+++ script/start.sh
@@ -10,7 +10,7 @@ for item in ${CONFIG[@]}; do
   redis_key=$(echo ${item} | awk -F ':' '{print $5}')
 
   time_day=$(date "+%Y%m%d")
-  log_path="${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}/${time_day}"
+  log_path="${PROJECT_CONSOLE_LOG_PATH}/${time_day}/${server_host}/${client_id}"
   if [ ! -d "${log_path}" ]; then
     mkdir -p ${log_path}
   fi
@@ -22,7 +22,7 @@ for item in ${CONFIG[@]}; do
   else
     echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: start to running." >>"${log_path}/${PROJECT_NAME}.log"
 
-    time_hour=$(date "%H%M")
+    time_hour=$(date "+%H%M")
     log_path="${log_path}/${time_hour}.log"
 
     nohup java -jar ${PROJECT_NAME_JAR} \
--- script/stop.sh
+++ script/stop.sh
@@ -8,11 +8,17 @@ for item in ${CONFIG[@]}; do
   server_host=`echo ${item} | awk -F ':' '{print $2}'`
   client_id=`echo ${item} | awk -F ':' '{print $4}'`
 
+  time_day=$(date "+%Y%m%d")
+  log_path="${PROJECT_CONSOLE_LOG_PATH}/${time_day}/${server_host}/${client_id}"
+  if [ ! -d "${log_path}" ]; then
+    mkdir -p ${log_path}
+  fi
+
   num=$(ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${active}" | grep "${client_id}" | grep -v "grep" | wc -l)
   if [ $num -gt 0 ]; then
     ps -ef | grep "${PROJECT_NAME_JAR}" | grep "${client_id}" | grep "${client_id}" | grep -v "grep" | awk '{print $2}' | xargs kill -9
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >>"${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: to stop." >>"${log_path}/${PROJECT_NAME}.log"
   else
-    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >>"${PROJECT_CONSOLE_LOG_PATH}/${server_host}.${client_id}.${PROJECT_NAME}.log"
+    echo "[${CUR_TIME}]-${PROJECT_NAME_JAR}: not running." >>"${log_path}/${PROJECT_NAME}.log"
   fi
 done
